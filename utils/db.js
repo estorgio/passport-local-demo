3d7ded79d8a6ee9d5ef56eeb17636eacb8af3a99
Fortunato Estorgio <contact@estorgio.com>
2019-05-01 16:55:28 +0800

    Handle database disconnect

--- utils/db.js
+++ utils/db.js
@@ -5,14 +5,20 @@ require('dotenv').config();
 const connectionString = process.env.DB_CONNECTION;
 
 function connect() {
+  const options = {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    bufferCommands: false,
+    bufferMaxEntries: 0,
+    reconnectTries: Number.MAX_VALUE,
+  };
   mongoose
-    .set('useNewUrlParser', true)
-    .set('useFindAndModify', false)
-    .connect(connectionString)
+    .connect(connectionString, options)
     .then(() => console.log('Connected to the database'))
     .catch((err) => {
       console.log('An error occured while trying to connect to the database.');
       console.log(`${err.name}: ${err.message}`);
+      process.exit(1);
     });
 }
 
